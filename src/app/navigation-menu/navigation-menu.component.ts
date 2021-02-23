7f8b9fd4c8b947470770bb276b58f32a219158bb
Ivan <ivansergienko3211@gmail.com>
2021-02-23 21:01:23 +0100

    Styling the navigation menu

--- src/app/navigation-menu/navigation-menu.component.ts
+++ src/app/navigation-menu/navigation-menu.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-navigation-menu',
@@ -8,7 +9,7 @@ import { Component, OnInit } from '@angular/core';
 export class NavigationMenuComponent implements OnInit {
   navbarBurgerActive = false;
 
-  constructor() { }
+  constructor(public router: Router) { }
 
   ngOnInit(): void {
   }
