50dfbf5098ed0baaf1fe9d6b22e8f3230ae80374
Ivan <ivansergienko3211@gmail.com>
2021-02-26 16:36:50 +0100

    Adding Font Awesome icons

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { ArticleListComponent } from './article-list/article-list.component';
 import { NavigationMenuComponent } from './navigation-menu/navigation-menu.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AboutComponent } from './about/about.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { AboutComponent } from './about/about.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
