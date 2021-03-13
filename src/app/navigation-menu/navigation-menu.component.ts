39364ccdc4761e7aafc7627e45db8f5e8672863e
Ivan <ivansergienko3211@gmail.com>
2021-03-13 17:26:05 +0100

    Theme switcher service

--- src/app/navigation-menu/navigation-menu.component.ts
+++ src/app/navigation-menu/navigation-menu.component.ts
@@ -1,7 +1,8 @@
 import {Component, OnInit} from '@angular/core';
 import {Router} from '@angular/router';
-import {faHome, faMoon, faRadiationAlt, faSun, IconDefinition} from '@fortawesome/free-solid-svg-icons';
+import {faHome, IconDefinition} from '@fortawesome/free-solid-svg-icons';
 import {faGithub, faLinkedin} from '@fortawesome/free-brands-svg-icons';
+import {ThemeConfig, ThemeSwitcherService} from '../theme-switcher.service';
 
 @Component({
   selector: 'app-navigation-menu',
@@ -14,52 +15,21 @@ export class NavigationMenuComponent implements OnInit {
   faHome: IconDefinition = faHome;
   faGithub: IconDefinition = faGithub;
   faLinkedin: IconDefinition = faLinkedin;
-  // Active theme
+  // Theme
   theme: ThemeConfig;
 
-  private themes: Dictionary<ThemeConfig>;
-
-  constructor(public router: Router) {
+  constructor(public router: Router, public themeSwitcher: ThemeSwitcherService) {
   }
 
   ngOnInit(): void {
-    this.themes = new Dictionary<ThemeConfig>();
-    this.themes.light = {
-      cssPath: '/assets/css/bulma.min.css',
-      nextThemeName: 'dark',
-      nextThemeIcon: faSun
-    };
-    this.themes.dark = {
-      cssPath: '/assets/css/darkly.min.css',
-      nextThemeName: 'nuclear',
-      nextThemeIcon: faMoon
-    };
-    this.themes.nuclear = {
-      cssPath: '/assets/css/nuclear.min.css',
-      nextThemeName: 'light',
-      nextThemeIcon: faRadiationAlt
-    };
+    this.theme = this.themeSwitcher.defaultTheme();
+  }
 
-    this.switchTheme(localStorage.getItem('theme') || 'dark');
+  switchTheme(): void {
+    this.theme = this.themeSwitcher.nextTheme(this.theme);
   }
 
   toggleNavbar(): void {
     this.navbarBurgerActive = !this.navbarBurgerActive;
   }
-
-  switchTheme(themeName: string): void {
-    this.theme = this.themes[themeName];
-    localStorage.setItem('theme', themeName);
-    document.getElementById('global-theme').setAttribute('href', this.theme.cssPath);
-  }
-}
-
-class Dictionary<T> {
-  [key: string]: T;
-}
-
-export interface ThemeConfig {
-  cssPath: string;
-  nextThemeName: string;
-  nextThemeIcon: IconDefinition;
 }
---
+++ src/app/theme-switcher.service.ts
@@ -0,0 +1,60 @@
+import {Injectable} from '@angular/core';
+import {faMoon, faRadiationAlt, faSun, IconDefinition} from '@fortawesome/free-solid-svg-icons';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ThemeSwitcherService {
+  // Active theme
+  private readonly themes: Dictionary<ThemeConfig>;
+
+  constructor() {
+    this.themes = new Dictionary<ThemeConfig>();
+    this.themes.light = {
+      name: 'light',
+      cssPath: '/assets/css/bulma.min.css',
+      nextThemeName: 'dark',
+      nextThemeIcon: faSun
+    };
+    this.themes.dark = {
+      name: 'dark',
+      cssPath: '/assets/css/darkly.min.css',
+      nextThemeName: 'nuclear',
+      nextThemeIcon: faMoon
+    };
+    this.themes.nuclear = {
+      name: 'nuclear',
+      cssPath: '/assets/css/nuclear.min.css',
+      nextThemeName: 'light',
+      nextThemeIcon: faRadiationAlt
+    };
+  }
+
+  defaultTheme(): ThemeConfig {
+    const theme = this.themes[localStorage.getItem('theme')] || this.themes.light;
+    this.setTheme(theme);
+    return theme;
+  }
+
+  setTheme(theme: ThemeConfig): void {
+    localStorage.setItem('theme', theme.name);
+    document.getElementById('global-theme').setAttribute('href', theme.cssPath);
+  }
+
+  nextTheme(theme: ThemeConfig): ThemeConfig {
+    const next = this.themes[theme.nextThemeName];
+    this.setTheme(next);
+    return next;
+  }
+}
+
+class Dictionary<T> {
+  [key: string]: T;
+}
+
+export interface ThemeConfig {
+  name: string;
+  cssPath: string;
+  nextThemeName: string;
+  nextThemeIcon: IconDefinition;
+}
